4b2b566b2ce9a226fd7c5a1e10113d6c8c716695
hackhub817 <piyushguptaji123@gmail.com>
2024-10-29 19:04:26 +0530

    deply

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://project-deploy-front.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
