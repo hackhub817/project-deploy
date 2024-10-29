71ee3f6d76f95a10837048d06bcdadda9aa09e61
hackhub817 <piyushguptaji123@gmail.com>
2024-10-29 20:02:47 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: "https://project-deploy-front.vercel.app",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   })
 );
 
